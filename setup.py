1a5f1ac4f565aad14d205682e6b4a4e6aa1c1c4d
Makoto <FujiMakoto@users.noreply.github.com>
2015-09-13 11:11:08 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IPS Vagrant',
+    version='0.1.0',
+    description='IPS Vagrant',
+    long_description='IPS Vagrant',
+    author='Makoto Fujimoto',
+    author_email='makoto@makoto.io',
+    url='https://github.com/FujiMakoto/IPS-Vagrant',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=find_packages(),
+    # entry_points={
+    #     '': ['']
+    # },
+    requires=['']
+)
