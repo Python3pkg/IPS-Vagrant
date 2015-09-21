048adc5f01069badd208237217652dda14454d35
Makoto <FujiMakoto@users.noreply.github.com>
2015-09-21 02:46:59 -0400

    Installer documentation

--- ips_vagrant/scraper/installer.py
+++ ips_vagrant/scraper/installer.py
@@ -19,9 +19,9 @@ class Installer(object):
         """
         Initialize a new Installer instance
         @type   ctx:    ips_vagrant.cli.Context
+        @param  site:   The IPS Site we are installing
         @type   site:   ips_vagrant.models.sites.Site
         """
-        # Debug log
         self.log = logging.getLogger('ipsv.installer')
         self._previous_title = None
         self.url = '{scheme}://{host}/admin/install'.format(
@@ -37,19 +37,26 @@ class Installer(object):
         self.browser.set_cookiejar(self.cookiejar)
 
     def _check_title(self, title):
+        """
+        If we're on the same page, we got an error and need to raise an exception
+        @type   title:  str
+        @raise  InstallationError:  Title matches the previous page requests title (We're on the same page)
+        """
         self.log.info('Installation page loaded: %s', title)
         if self._previous_title and title == self._previous_title:
             raise InstallationError('Unexpected page error')
 
     def start(self):
         """
-        Check if we have an active login session set
-        @rtype: bool
+        Start the installation wizard
         """
         self.log.debug('Starting the installation process')
         self.system_check()
 
     def system_check(self):
+        """
+        System requirements check
+        """
         self.browser.open(self.url)
         self._check_title(self.browser.title())
         rsoup = BeautifulSoup(self.browser.response().read())
@@ -69,12 +76,15 @@ class Installer(object):
         self.license()
 
     def license(self):
+        """
+        Submit our license to IPS' servers
+        """
         self._check_title(self.browser.title())
         self.browser.select_form(nr=0)
 
         # Set the fields
         self.browser.form['lkey'] = '{license}-TESTINSTALL'.format(license=self.site.license_key)
-        self.browser.find_control('eula_checkbox').items[0].selected = True
+        self.browser.find_control('eula_checkbox').items[0].selected = True  # TODO: User prompt?
 
         # Submit the request
         self.log.debug('Submitting our license')
@@ -84,6 +94,9 @@ class Installer(object):
         self.applications()
 
     def applications(self):
+        """
+        Select the applications to install (currently hardcoded to install all applications)
+        """
         # Check for license submission errors
         try:
             self._check_title(self.browser.title())
@@ -98,6 +111,9 @@ class Installer(object):
         self.server_details()
 
     def server_details(self):
+        """
+        Input server details (database information, etc.)
+        """
         self._check_title(self.browser.title())
 
         # Create the database
@@ -113,11 +129,13 @@ class Installer(object):
         self.mysql.execute("GRANT ALL ON {db}.* TO '{u}'@'localhost' IDENTIFIED BY '{p}'"
                            .format(db=db_name, u=db_user, p=db_pass))
 
+        # Save the database connection information
         self.site.db_host = 'localhost'
         self.site.db_name = db_name
         self.site.db_user = db_user
         self.site.db_pass = db_pass
 
+        # Set form fields and submit
         self.browser.select_form(nr=0)
         self.browser.form['sql_host'] = 'localhost'
         self.browser.form['sql_user'] = db_user
@@ -127,38 +145,47 @@ class Installer(object):
         self.admin()
 
     def admin(self):
+        """
+        Provide admin login credentials
+        """
         self._check_title(self.browser.title())
         self.browser.select_form(nr=0)
 
-        self.browser.form['admin_user'] = 'makoto'  # click.prompt('Admin display name')
-        password = 'makoto'  # click.prompt('Admin password', confirmation_prompt='Confirm admin password')
+        self.browser.form['admin_user'] = click.prompt('Admin display name')
+        password = click.prompt('Admin password', confirmation_prompt='Confirm admin password')
         self.browser.form['admin_pass1'] = password
         self.browser.form['admin_pass2'] = password
-        self.browser.form['admin_email'] = 'makoto@makoto.io'  # click.prompt('Admin email')
+        self.browser.form['admin_email'] = click.prompt('Admin email')
         self.browser.submit()
         self.install()
 
     def install(self):
+        """
+        Run the actual installation
+        """
         self._check_title(self.browser.title())
         continue_link = next(self.browser.links(text_regex='Start Installation'))
         self.browser.follow_link(continue_link)
 
+        # Get the MultipleRedirect URL
         rsoup = BeautifulSoup(self.browser.response().read())
-        cj = self.browser._ua_handlers['_cookies'].cookiejar
+        cj = self.browser._ua_handlers['_cookies'].cookiejar  # TODO
         mr_link = rsoup.find('div', {'class': 'ipsMultipleRedirect'})['data-url']
-        # mr_link = mr_link.encode('UTF-8')
         mr_link += '&' + urlencode({'mr': 'MA=='})
         self.log.debug('MultipleRedirect link: %s', mr_link)
 
+        # Set up the progress bar
         pbar = ProgressBar(100, 'Running installation...')
         pbar.start()
 
+        # Set up our requests session and get begin the installation
         s = requests.Session()
         s.headers.update({'X-Requested-With': 'XMLHttpRequest'})
         s.cookies.update(cj)
         r = s.get(mr_link)
         j = json.loads(r.text)
 
+        # Loop until we get a redirect json response
         while True:
             mr_link += '&' + urlencode({'mr': j[0]})
 
@@ -175,17 +202,15 @@ class Installer(object):
             r = s.get(mr_link)
             j = json.loads(r.text)
             self.log.debug('MultipleRedirect JSON response: %s', str(j))
-            pbar.update(min([progress, 100]), stage)
+            pbar.update(min([progress, 100]), stage)  # NOTE: Response may return progress values above 100
 
+            # We're done, finalize the installation and break
             if 'redirect' in j:
                 pbar.finish()
                 break
 
         self.log.info('Finalizing installation')
-        r = s.get(j['redirect'])
-
-        with open('output.html', 'w') as f:
-            f.write(r.text)
+        s.get(j['redirect'])
 
 
 class InstallationError(Exception):
